a3005ea4306c8be2c7148cd8d464825a91efd34f
Pixellly <fs.karaa@gmail.com>
2025-07-10 12:45:24 +0400

    Added playable buttons and game status is displayed on wbesite instead of console

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
+let humanScore = 0
+let computerScore = 0
+
+const body = document.querySelector('body')
+
+const results = document.createElement('div')
+const rock = document.createElement('button')
+const paper = document.createElement('button')
+const scissors = document.createElement('button')
+
+rock.innerText ='rock'
+paper.innerText = 'paper'
+scissors.innerText = 'scissors'
+
+body.appendChild(results)
+body.appendChild(rock)
+body.appendChild(paper)
+body.appendChild(scissors)
+
+
+
+//randomized choice
 function getComputerChoice(){
     let choice = Math.floor(Math.random() * 3);
     if( choice == 0) return 'rock'
@@ -5,42 +27,64 @@ function getComputerChoice(){
     else return 'scissors'
 }
 
+/*choice on click
 function getHumanChoice() {
     let choice = prompt("Choose “rock”, “paper” or “scissors”: ")
     return choice.toLowerCase() 
 }
+*/
 
-
-function playGame(){
-    let humanScore = 0
-    let computerScore = 0
-
-    function playRound(humanChoice, computerChoice) {
-        if(humanChoice === computerChoice){ console.log('It is a draw!')
+function playRound(humanChoice, computerChoice) {
+        results.innerText = ''
+        if(humanChoice === computerChoice){ results.innerText += 'It is a draw!'
 
         } else if (
             humanChoice === 'paper' && computerChoice==='scissors' ||
             humanChoice === 'rock' && computerChoice==='paper' ||
             humanChoice === 'scissors' && computerChoice==='rock') {
-            console.log(`You lose! ${computerChoice} beats ${humanChoice}`)
+            results.innerText += `You lose! ${computerChoice} beats ${humanChoice}`
             computerScore ++
         } else {
-            console.log(`You win! ${humanChoice} beats ${computerChoice}`)
+            results.innerText +=  `You win! ${humanChoice} beats ${computerChoice}`
             humanScore ++
         }
-    }
+        updateGame()
+}
 
-    for(let i=1; i<=5; i++){
+function playGame(){
+    /*for(let i=1; i<=5; i++){
         const humanSelection = getHumanChoice();
         const computerSelection = getComputerChoice();
 
         playRound(humanSelection, computerSelection);
-    }
-
+    }*/
+    
+    rock.addEventListener('click', () => playRound('rock', getComputerChoice()))
+    paper.addEventListener('click', () => playRound('paper', getComputerChoice()))
+    scissors.addEventListener('click', () => playRound('scissors', getComputerChoice()))
+    
     //declare winner
+    
+    /*
     if (humanScore > computerScore) { console.log('You won this match!')}
     else if (humanScore < computerScore) { console.log('Computer won this match!')}
     else { console.log('No winner!')}
+    */
+    }
+
+function updateGame(){
+    if(humanScore <5  && computerScore<5) {
+    results.innerText += `\nRunning Score: ${humanScore}/${computerScore}`
+    }
+    else if(humanScore>computerScore){
+        results.innerText += `\nRunning Score: ${humanScore}/${computerScore}
+        human won!`
+    }
+    else{
+        results.innerText += `\nRunning Score: ${humanScore}/${computerScore}
+        computer won!`  
+    }
 }
+
 
 playGame()
